ddc953c91684bcc0c7af90b9a6543dac22a5fb94
libscode <lmtlibasse@gmail.com>
2022-10-26 19:05:34 +0000

    page add product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
+use App\Entity\Category;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
@@ -45,5 +51,41 @@ class ProductController extends AbstractController
             'product' => $product
         ]);
 
+    }
+
+    //Page ajouter un nouveau produit
+    #[Route('/admin/product/create', name:'product_create')]
+    public function create(FormFactoryInterface $factory){
+        
+        $builder = $factory->createBuilder();
+
+        $builder->add('name', TextType::class, [
+            
+        ])
+                ->add('shortDescription', TextareaType::class, [
+                    'attr' => [
+                        'placeholder' => 'donner une couter description du produit'
+                    ]
+                ])
+                ->add('price', MoneyType::class, [
+                    'attr' => [
+                        'placeholder' => 'donez le prix du produit'
+                    ]
+                ])
+                ->add('category', EntityType::class, [
+                    'attr' => [
+                        'placeholder' => 'choisir une catégorie'
+                    ],
+                    'class'       => Category::class,
+                    'choice_label'=> 'name'
+                ]);
+        
+        $form = $builder->getForm();
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
     }
 }
